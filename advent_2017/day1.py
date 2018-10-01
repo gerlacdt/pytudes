a1e30db71459038350e7894e8b1b0e36ac53be00
Daniel Gerlach <daniel.gerlach1981@googlemail.com>
2018-10-01 21:55:03 +0200

    applied norvigs solution to advent day1 problem

--- advent_2017/day1.py
+++ advent_2017/day1.py
@@ -3,12 +3,14 @@ def toList(s):
 
 
 def captcha(s):
-    seq = toList(s)
-    seq = seq + [seq[0]]
-    acc = 0
-    for idx, val in enumerate(seq):
-        if idx == len(seq) - 1:
-            break
-        if val == seq[idx + 1]:
-            acc += val
-    return acc
+    digits = toList(s)
+    return sum(digits[i] for i in range(len(digits))
+               if digits[i] == digits[i-1])
+    # seq = seq + [seq[0]]
+    # acc = 0
+    # for idx, val in enumerate(seq):
+    #     if idx == len(seq) - 1:
+    #         break
+    #     if val == seq[idx + 1]:
+    #         acc += val
+    # return acc
