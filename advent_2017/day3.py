11de5249a744efa3656dd6b595e028f908f8f0f5
Daniel Gerlach <daniel.gerlach1981@googlemail.com>
2018-10-03 21:02:31 +0200

    fixed advent puzzle 3, now generates a grid from a number n

--- advent_2017/day3.py
+++ advent_2017/day3.py
@@ -1,14 +1,95 @@
 import utils
+import math
 
 
-def findCenter(arr):
-    '''Finds the center of the matrix. Center is defined where the value 1
-is.'''
-    for x, line in enumerate(arr):
-        for y, val in enumerate(line):
-            if val == 1:
-                return x, y
-    raise RuntimeError('No center exist with value 1')
+def move(position, direction):
+    '''Move one step from the given to position to the given direction.'''
+    x, y = position
+    if direction == 'up':
+        return (x - 1, y)
+    elif direction == 'down':
+        return (x + 1, y)
+    elif direction == 'left':
+        return (x, y - 1)
+    elif direction == 'right':
+        return (x, y + 1)
+    raise RuntimeError("Given directions does not exist: {}".format(direction))
+
+
+def nextDirection(direction):
+    '''Return the next direction if a direction change is requested.'''
+    if direction == 'up':
+        return 'left'
+    elif direction == 'down':
+        return 'right'
+    elif direction == 'left':
+        return 'down'
+    elif direction == 'right':
+        return 'up'
+    raise RuntimeError("Given directions does not exist: {}".format(direction))
+
+
+def grid(n):
+    '''generates a spiral grid with 1 as center number'''
+    if n % 2 == 0:
+        raise RuntimeError("Given grid-size must be an odd number: {}.format(n)")
+
+    currentStepsLeft = 0
+    currentDirection = 'down'  # initial direction, switch in first iteration to 'right
+    maxSteps = 0
+
+    # initial empty grid with center number 1
+    center = n - 1
+    currentPosition = (center // 2, center // 2)
+    grid = [[0 for i in range(center + 1)] for j in range(center + 1)]
+    grid[currentPosition[0]][currentPosition[1]] = 1
+
+    # fill zero grid with numbers
+    for i in range(2, (n ** 2) + 1):
+        if currentStepsLeft == 0:
+            currentDirection = nextDirection(currentDirection)
+            if currentDirection == 'left' or currentDirection == 'right':
+                maxSteps += 1
+            currentStepsLeft = maxSteps
+        currentPosition = move(currentPosition, currentDirection)
+        x, y = currentPosition
+        grid[x][y] = i
+        currentStepsLeft -= 1
+    return grid
+
+
+def spiral(qn):
+    '''Returns the manhatten distance for a given number qn in a spriral
+grid which is generated from n. The the grid will have n^2 fields. n
+must be odd in order to generate a correct spiral from the inner to
+outside with the start value of 1.
+
+Example:
+spriral(33, 1023) => 31
+spriral(5, 1) => 0
+spriral(5, 17) => 4
+    '''
+    # find grid size
+    n = int(math.sqrt(qn))
+    n += 1
+    if n % 2 == 0:
+        n += 1
+
+    g = grid(n)
+    center = (n // 2, n // 2)
+    # find requested number in grid
+    position = ()
+    for i, line in enumerate(g):
+        for j, val in enumerate(line):
+            if val == qn:
+                position = (i, j)
+
+    if position == ():
+        raise RuntimeError("requested number not in grid")
+
+    # calc distance from center
+    dist = calcManhattanDistance(center, position)
+    return dist
 
 
 def calcManhattanDistance(orig, dest):
@@ -17,23 +98,3 @@ tuple'''
     ox, oy = orig[0], orig[1]
     dx, dy = dest[0], dest[1]
     return abs(ox - dx) + abs(oy - dy)
-
-
-def spiral(s):
-    '''Returns the sum of all steps needed to bring all numbers to the
-center.'''
-    rows = utils.Array(s)
-    ox, oy = findCenter(rows)
-
-    # for all from 0,0 find way to 1, count steps
-    acc = 0
-    for x, line in enumerate(rows):
-        for y, val in enumerate(line):
-            if val == 1:
-                continue
-            dist = calcManhattanDistance((ox, oy), (x, y))
-            print("({}, {},  val: {} => dist {})".format(x, y, val, dist))
-            acc += dist
-
-    # sum all steps return from all numbers
-    return acc
--- advent_2017/test_day3.py
+++ advent_2017/test_day3.py
@@ -1,44 +1,9 @@
 import day3
-import utils
 import unittest
 
 
 class TestDay3(unittest.TestCase):
-    s = '''17  16  15  14  13
-18   5   4   3  12
-19   6   1   2  11
-20   7   8   9  10
-21  22  23
-'''
-
-    def test_findCenter(self):
-        rows = utils.Array(self.s)
-        x, y = day3.findCenter(rows)
-        ex, ey = 2, 2
-        self.assertEqual(x, ex)
-        self.assertEqual(y, ey)
-
-    def test_findCenter_fail(self):
-        s = '''2 3 4
-5 6 7
-'''
-        with self.assertRaises(RuntimeError):
-            rows = utils.Array(s)
-            x, y = day3.findCenter(rows)
-
-    def test_dist(self):
-        rows = utils.Array(self.s)
-        x, y = day3.findCenter(rows)
-
-        # test cases, (dest-x,dest-y, distance)
-        tests = ((2, 2, 0), (4, 1, 3), (2, 4, 2))
-
-        for t in tests:
-            result = day3.calcManhattanDistance((x, y), (t[0], t[1]))
-            expected = t[2]
-            self.assertEqual(result, expected)
-
     def test_spiral(self):
-        result = day3.spiral(self.s)
-        expected = 53
+        result = day3.spiral(277678)
+        expected = 475
         self.assertEqual(result, expected)
