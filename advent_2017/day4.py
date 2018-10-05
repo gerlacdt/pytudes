149e28ae531c4b2f3bfa375661a8ec8f442a9cf3
Daniel Gerlach <daniel.gerlach1981@googlemail.com>
2018-10-05 22:24:12 +0200

    solved day4 part 2

--- advent_2017/day4.py
+++ advent_2017/day4.py
@@ -8,14 +8,17 @@ def readFile(filename):
     return content
 
 
-def isValid(s):
+def isValid(s, anagram=False):
     '''Accepts a long string with multiple lines and checks how many lines
 are valid passphrases. A passphrase/line is valid if a line does not
 contain duplicate words.
     '''
-    return len([line for line in s.splitlines() if noDuplicate(line)])
+    return len([line for line in s.splitlines() if noDuplicate(line, anagram)])
 
 
-def noDuplicate(line):
-    counts = Counter(line.split())
+def noDuplicate(line, anagram=False):
+    words = line.split()
+    if anagram:
+        words = [''.join(sorted(w)) for w in words]
+    counts = Counter(words)
     return all([value == 1 for key, value in counts.items()])
--- advent_2017/test_day4.py
+++ advent_2017/test_day4.py
@@ -3,9 +3,23 @@ import unittest
 
 
 class TestDay4(unittest.TestCase):
-    def test_passphrase(self):
+    @classmethod
+    def setUpClass(self):
         filename = 'data/input4.txt'
-        content = day4.readFile(filename)
-        result = day4.isValid(content)
+        self.content = day4.readFile(filename)
+
+    def test_passphrase(self):
+        result = day4.isValid(self.content)
         expected = 383
+        self.assertEqual(result, expected)
+
+    def test_passphrase_anagram(self):
+#         content = '''abcde fghij
+# abcde xyz ecdab
+# a ab abc abd abf abj
+# iiii oiii ooii oooi oooo
+# oiii ioii iioi iiio
+# '''
+        result = day4.isValid(self.content, True)
+        expected = 265
         self.assertEqual(result, expected)
