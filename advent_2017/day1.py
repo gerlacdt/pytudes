2dcd15682b1b37899f37a314b65b119104bdfa25
Daniel Gerlach <daniel.gerlach1981@googlemail.com>
2018-10-07 14:56:17 +0200

    solved second parts of day 1 and 2 puzzles

--- advent_2017/day1.py
+++ advent_2017/day1.py
@@ -6,3 +6,10 @@ def captcha(s):
     digits = toList(s)
     return sum(digits[i] for i in range(len(digits))
                if digits[i] == digits[i-1])
+
+
+def captcha2(s):
+    digits = toList(s)
+    steps = int(len(digits) / 2)
+    return sum(digits[i] for i in range(len(digits))
+               if digits[i-steps] == digits[i])
--- advent_2017/day2.py
+++ advent_2017/day2.py
@@ -10,3 +10,11 @@ def checksum(s):
 def norvig_checksum(rows):
     rows2 = utils.Array(rows)
     return sum(abs(max(row) - min(row)) for row in rows2)
+
+
+def checksum2(rows):
+    rows2 = utils.Array(rows)
+    return sum([int(x / y) for row in rows2
+                for i, x in enumerate(row)
+                for j, y in enumerate(row)
+                if i != j and x % y == 0])
