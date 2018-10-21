cbc64a3b11ecb3ded9c091222a0ea3133b1a6e4a
Daniel Gerlach <daniel.gerlach1981@googlemail.com>
2018-10-21 19:52:10 +0200

    applied numba.jit for faster execution

--- advent_2017/day15.py
+++ advent_2017/day15.py
@@ -1,14 +1,16 @@
+from numba import jit
+
+
 def judge(startA, startB, factorA=16807, factorB=48271, multipliesA=1,
           multipliesB=1, N=5):
     genA = gen(startA, factorA, multipliesA)
     genB = gen(startB, factorB, multipliesB)
+    mask = 2**16-1
     acc = 0
     for i in range(N):
         valA = next(genA)
         valB = next(genB)
-        binA = toBin(valA)
-        binB = toBin(valB)
-        if binA[16:] == binB[16:]:
+        if valA & mask == valB & mask:
             acc += 1
     return acc
 
@@ -23,5 +25,14 @@ def gen(start, factor, multiplies=1):
             yield val
 
 
-def toBin(n):
-    return "{0:032b}".format(n)
+@jit
+def duelgen(prev1=591, factor1=16807, prev2=393, factor2=48271,
+            m=2147483647, mask=2**16-1, N=40*10**6):
+    """numba.jit does not work with generators. So this is the rewrite
+from part 1."""
+    matches = 0
+    for _ in range(N):
+        prev1 = (prev1 * factor1) % m
+        prev2 = (prev2 * factor2) % m
+        matches += (prev1 & mask == prev2 & mask)
+    return matches
--- advent_2017/test_day15.py
+++ advent_2017/test_day15.py
@@ -1,18 +1,16 @@
-from day15 import judge
+from day15 import judge, duelgen
 import unittest
 
 
 class TestDay15(unittest.TestCase):
-    # def test_judge(self):
+    def test_judge(self):
+        result = duelgen()
+        expected = 619
+        self.assertEqual(result, expected)
+
+    # def test_judge_2(self):
     #     genA = 591
     #     genB = 393
-    #     result = judge(genA, genB, N=40000000)
-    #     expected = 619
+    #     result = judge(genA, genB, multipliesA=4, multipliesB=8, N=5*10**6)
+    #     expected = 290
     #     self.assertEqual(result, expected)
-
-    def test_judge_2(self):
-        genA = 591
-        genB = 393
-        result = judge(genA, genB, multipliesA=4, multipliesB=8, N=5000000)
-        expected = 290
-        self.assertEqual(result, expected)
