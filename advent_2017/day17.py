7ea3d888bd566ca5c5650f4fc5ad839cd4a82643
Daniel Gerlach <daniel.gerlach1981@googlemail.com>
2018-10-25 23:21:50 +0200

    solved part 2 of day 17, but with norvig's help

--- advent_2017/day17.py
+++ advent_2017/day17.py
@@ -9,3 +9,13 @@ def spinlock(steps=3, N=2017):
         pos = index + 1
         d.insert(pos, i)
     return d[pos + 1]
+
+
+def angry_spinlock(steps=324, N=5):
+    buf = [0, 0]
+    pos = 0
+    for i in range(1, N + 1):
+        pos = (pos + steps) % i + 1
+        if pos <= 1:
+            buf[pos] = i
+    return buf[1]
--- advent_2017/test_day17.py
+++ advent_2017/test_day17.py
@@ -7,3 +7,8 @@ class TestDay17(unittest.TestCase):
         result = day17.spinlock(324)
         expected = 1306
         self.assertEqual(result, expected)
+
+    def test_angry_spinlock(self):
+        result = day17.angry_spinlock(324, N=50*10**6)
+        expected = 20430489
+        self.assertEqual(result, expected)
