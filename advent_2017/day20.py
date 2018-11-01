88649a62be2f75504e1a1fc6864903dad390d415
Daniel Gerlach <daniel.gerlach1981@googlemail.com>
2018-11-01 11:22:16 +0100

    solved part2 day20

--- advent_2017/day20.py
+++ advent_2017/day20.py
@@ -1,5 +1,6 @@
 from utils import Integers
 from myutils import readFile
+from collections import defaultdict
 
 
 INPUT1 = readFile("./data/input20.txt")
@@ -27,28 +28,54 @@ def tick(particles):
     return [distance((p[0], p[1], p[2])) for p in particles]
 
 
-def swarm(input1=input1):
+def swarm(input1=input1, N=100):
     lines = input1.splitlines()
     particles = [Integers(line) for line in lines]
     distances = [distance((p[0], p[1], p[2])) for p in particles]
     minIndex = -1
 
-    for i in range(1000):
+    for i in range(N):
         distances = tick(particles)
         minIndex = distances.index(min(distances))
 
     return minIndex
 
 
-def collision(input1):
+input2 = """p=<-6,0,0>, v=< 3,0,0>, a=< 0,0,0>
+p=<-4,0,0>, v=< 2,0,0>, a=< 0,0,0>
+p=<-2,0,0>, v=< 1,0,0>, a=< 0,0,0>
+p=< 3,0,0>, v=<-1,0,0>, a=< 0,0,0>
+"""
+
+
+def destroy(particles):
+    # check collisions of particles
+    # remove collided particles
+    dups = defaultdict(set)
+    for i, p in enumerate(particles):
+        dups[(p[0], p[1], p[2])].add(i)
+
+    indexToDelete = set()
+    for k, v in dups.items():
+        l = dups[k]
+        if len(l) > 1:
+            indexToDelete.update(l)
+
+    clearedParticles = []
+    for i, p in enumerate(particles):
+        if i in indexToDelete:
+            continue
+        clearedParticles.append(p)
+    return clearedParticles
+
+
+def collision(input1, N=3):
     lines = input1.splitlines()
     particles = [Integers(line) for line in lines]
     distances = [distance((p[0], p[1], p[2])) for p in particles]
     minIndex = -1
 
-    for i in range(1000):
+    for i in range(N):
         tick(particles)
-        # check collisions of particles
-        # remove collided particles
-
+        particles = destroy(particles)
     return len(particles)
--- advent_2017/test_day20.py
+++ advent_2017/test_day20.py
@@ -1,9 +1,14 @@
-from day20 import swarm, INPUT1
+from day20 import swarm, collision, INPUT1
 import unittest
 
 
 class TestDay20(unittest.TestCase):
     def test_swarm(self):
-        result = swarm(INPUT1)
-        expected = 0
+        result = swarm(INPUT1, 500)
+        expected = 150
+        self.assertEqual(result, expected)
+
+    def test_collision(self):
+        result = collision(INPUT1, 100)
+        expected = 657
         self.assertEqual(result, expected)
