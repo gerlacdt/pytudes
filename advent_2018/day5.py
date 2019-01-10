af99603f05fd472ed201d30b5fe3fd0df1d9d1c8
Daniel Gerlach <daniel.gerlach1981@googlemail.com>
2019-01-10 23:25:08 +0100

    added double linked list implementation for better time complexity during merge of adjacent items in collection

--- advent_2018/day5.py
+++ advent_2018/day5.py
@@ -12,33 +12,124 @@ def react(c1, c2):
 input5 = Inputstr(5)
 
 
-def chain_slow(arr):
-    current = 1
-    previous = 0
-    while current < len(arr):
-        if react(arr[previous], arr[current]):
-            arr = arr[:previous] + arr[current+1:]
-            previous -= 1
-            current -= 1
-        else:
-            previous += 1
-            current += 1
-    return len(arr)
+class Node():
+    def __init__(self, value, previous, succ):
+        self.value = value
+        self.previous = previous
+        self.succ = succ
+
+    def __str__(self):
+        return "{}, ({})".format(
+            self.value,  self.succ)
+
+
+class DoubleLinkedList():
+    def __init__(self, arr=[]):
+        self.head = None
+        self.tail = None
+        for item in arr:
+            self.append(item)
+
+    def insert(self, value):
+        if not self.head:
+            node = Node(value, None, None)
+            self.head = node
+            self.tail = node
+            return
+        node = Node(value, None, self.head)
+        self.head.previous = node
+        self.head = node
+
+    def append(self, value):
+        if not self.tail:
+            node = Node(value, None, None)
+            self.head = node
+            self.tail = node
+            return
+        node = Node(value, self.tail, None)
+        self.tail.succ = node
+        self.tail = node
+
+    def search(self, value):
+        current = self.head
+        while current:
+            if current.value == value:
+                return current
+            current = current.succ
+        return None
+
+    def to_list(self):
+        current = self.head
+        lst = []
+        while current:
+            lst.append(current.value)
+            current = current.succ
+        return lst
+
+    def to_list_reverse(self):
+        current = self.head
+        lst = []
+        while current.succ:
+            current = current.succ
+
+        while current:
+            lst.append(current.value)
+            current = current.previous
+        return lst
+
+    def delete(self, node):
+        if not node.previous and not node.succ:
+            self.head = None
+            self.tail = None
+            return
+
+        if not node.previous:
+            node.succ.previous = None
+            self.head = node.succ
+            return None
+
+        if not node.succ:
+            node.previous.succ = None
+            self.tail = node.previous
+            return None
+        node.previous.succ = node.succ
+        node.succ.previous = node.previous
+
+    def __str__(self):
+        return str(self.head)
+
+
+# def chain(arr):
+#     arr2 = arr[:]
+#     current = 1
+#     previous = 0
+#     while current < len(arr2):
+#         if react(arr2[previous], arr2[current]):
+#             arr2 = arr2[:previous] + arr2[current+1:]
+#             previous -= 1
+#             current -= 1
+#         else:
+#             previous += 1
+#             current += 1
+#     return len(arr2)
 
 
 def chain(arr):
-    arr2 = []
-    current = 1
-    previous = 0
-    for c in arr:
-        if react(arr[previous], arr[current]):
-            previous += 1
-            current += 1
-            continue
-        arr2.append(c)
-        previous += 1
-        current += 1
-    return len(arr2)
+    lst = DoubleLinkedList(arr)
+    current = lst.head.succ
+    previous = lst.head
+    while current:
+        if react(previous.value, current.value):
+            tmp_prev = previous
+            previous = previous.previous
+            current = current.succ
+            # print("delete prev: {}, prev_succ: {}".format(tmp_prev.value, tmp_prev.succ.value))
+            lst.delete(tmp_prev.succ)
+            lst.delete(tmp_prev)
+        else:
+            previous, current = current, current.succ
+        # print("".join(lst.to_list()))
+    return len(lst.to_list())
 
 
 def part1(content=test_input):
--- advent_2018/test_day5.py
+++ advent_2018/test_day5.py
@@ -1,6 +1,22 @@
-from day5 import part1
+from day5 import part1, DoubleLinkedList
 
 
 class TestDay5():
+    def test_double_linked_list(self):
+        myrange = 5
+        expected = [i for i in range(myrange)]
+        lst = DoubleLinkedList(expected)
+
+        assert lst.to_list() == expected
+        assert lst.to_list_reverse() == list(reversed(expected))
+
+        for i in range(myrange):
+            lst.delete(lst.search(i))
+
+            expected.remove(i)
+            assert lst.to_list() == expected
+
+
+
     def test_part1(self):
         pass
