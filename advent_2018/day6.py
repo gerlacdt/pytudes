c50fafa8174eac306028b631d3ce620fcb8ab8fc
Daniel Gerlach <daniel.gerlach1981@googlemail.com>
2019-01-13 10:57:34 +0100

    parse input 6

--- advent_2018/day6.py
+++ advent_2018/day6.py
@@ -1,7 +1,4 @@
-def manhatten_distance(p1, p2):
-    pass
-
-
+from utils import Inputstr, mapt
 
 test_input = """1, 1
 1, 6
@@ -11,5 +8,22 @@ test_input = """1, 1
 8, 9
 """
 
-def part1(content):
+input6 = Inputstr(6)
+
+
+def manhatten_distance(p1, p2):
+    """Returns the manhatten distance of the two given points. A point is
+a tuple with 2 integers."""
     pass
+
+
+def part1(content):
+    coordinates = [mapt(int, line.split(',')) for line in content.splitlines()]
+
+    # create grid-array
+
+    # places coordinates in array
+
+    # fill grid with coordinates and with 0 (0 means: does not belong
+    # to any coordinates)
+    return coordinates
