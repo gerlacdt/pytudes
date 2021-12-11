74157abcac6092110e37626d9851a7cbd19f9e1f
Daniel Gerlach <daniel.gerlach1981@googlemail.com>
2021-12-11 15:50:02 +0100

    day 11, use generator

--- advent_2021/day11.py
+++ advent_2021/day11.py
@@ -14,13 +14,6 @@ def toGrid(text):
     return grid
 
 
-def gridToString(grid):
-    s = ""
-    for row in grid:
-        s += "".join([str(val) for val in row]) + "\n"
-    return s[:-1]
-
-
 def isValid(grid, point):
     y, x = point
     maxY = len(grid) - 1
@@ -66,74 +59,35 @@ def flash(grid, start_points):
 
 
 def step(grid):
-    # increase by 1
-    for y, row in enumerate(grid):
-        for x, col in enumerate(row):
-            grid[y][x] += 1
+    while True:
+        # increase by 1
+        for y, row in enumerate(grid):
+            for x, col in enumerate(row):
+                grid[y][x] += 1
 
-    # chain reaction flash
-    start_points = []
-    for y, row in enumerate(grid):
-        for x, col in enumerate(row):
-            if col > 9:
-                start_points.append((y, x))
-    flashed = flash(grid, start_points)
+        # chain reaction flash
+        start_points = []
+        for y, row in enumerate(grid):
+            for x, col in enumerate(row):
+                if col > 9:
+                    start_points.append((y, x))
+        flashed = flash(grid, start_points)
 
-    # set to 0 for all val > 9
-    for y, row in enumerate(grid):
-        for x, col in enumerate(row):
-            if col > 9:
-                grid[y][x] = 0
-    return len(flashed)
+        # set to 0 for all val > 9
+        for y, row in enumerate(grid):
+            for x, col in enumerate(row):
+                if col > 9:
+                    grid[y][x] = 0
+        yield len(flashed)
 
 
 def count_flashes(grid, steps=100):
-    total_flushes = 0
-    for i in range(steps):
-        total_flushes += step(grid)
-
-    return total_flushes
+    return sum(next(step(grid)) for i in range(steps))
 
 
-def count_flashes_sync(grid, steps):
-    total_flushes = len(grid) * len(grid[0])
-    for i in range(steps):
-        if total_flushes == step(grid):
-            return i + 1
-
-    return None
-
-
-simple_example = """11111
-19991
-19191
-19991
-11111"""
-
-
-def test_simple_example():
-    expected_step1 = """34543
-40004
-50005
-40004
-34543"""
-
-    expected_step2 = """45654
-51115
-61116
-51115
-45654"""
-
-    # one step
-    grid = toGrid(simple_example)
-    actual = count_flashes(grid, 1)
-    assert actual == 9
-
-    # two steps
-    grid = toGrid(simple_example)
-    actual = count_flashes(grid, 2)
-    assert grid == toGrid(expected_step2)
-    assert actual == 9
+def count_flashes_sync(grid):
+    all_flushes = len(grid) * len(grid[0])
+    return next(i + 1 for i, flushes in enumerate(step(grid)) if all_flushes == flushes)
 
 
 example = """5483143223
@@ -146,40 +100,6 @@ example = """5483143223
 6882881134
 4846848554
 5283751526"""
-
-
-def test_count_flashes_example_steps():
-    grid = toGrid(example)
-    actual = count_flashes(grid, 1)
-
-    expected_step1 = """6594254334
-3856965822
-6375667284
-7252447257
-7468496589
-5278635756
-3287952832
-7993992245
-5957959665
-6394862637"""
-
-    assert gridToString(grid) == expected_step1, "step 1 failed"
-
-    expected_step2 = """8807476555
-5089087054
-8597889608
-8485769600
-8700908800
-6600088989
-6800005943
-0000007456
-9000000876
-8700006848"""
-
-    grid = toGrid(example)
-    actual = count_flashes(grid, 2)
-
-    assert gridToString(grid) == expected_step2, "step 2 failed"
 
 
 def test_count_flashes_example():
@@ -201,11 +121,11 @@ def test_count_flashes():
 
 def test_count_flashes_sync_example():
     grid = toGrid(example)
-    actual = count_flashes_sync(grid, 200)
+    actual = count_flashes_sync(grid)
     assert actual == 195
 
 
 def test_count_flashes_sync():
     grid = toGrid(getInput())
-    actual = count_flashes_sync(grid, 2000)
+    actual = count_flashes_sync(grid)
     assert actual == 276
