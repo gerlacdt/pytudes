e996fb361cdb044a2069e5d23a91d9de75fc482c
Daniel Gerlach <daniel.gerlach1981@googlemail.com>
2021-12-16 10:24:39 +0100

    removed double linked list

--- advent_2021/day14.py
+++ advent_2021/day14.py
@@ -3,78 +3,6 @@ import utils
 from collections import deque, Counter, namedtuple
 
 
-class Node:
-    def __init__(self, data, prev=None, next=None):
-        self.data = data
-        self.prev = prev
-        self.next = next
-
-    def __repr__(self):
-        return "{} -> {}".format(self.data, self.next)
-
-
-class DoubleLinkedList:
-    def __init__(self, arr=[]):
-        self.head: Node = None
-        self.tail: Node = None
-
-        for item in reversed(arr):
-            self.prepend(item)
-
-    def __iter__(self):
-        self.current = self.head
-        return self
-
-    def __next__(self):
-        if not self.current:
-            raise StopIteration
-        result = self.current
-        self.current = self.current.next
-        return result
-
-    def prepend(self, data):
-        if not self.head and not self.tail:
-            node = Node(data, None, None)
-            self.head = node
-            self.tail = node
-        else:
-            node = Node(data, None, self.head)
-            self.head.prev = node
-            self.head = node
-
-    def insert_after(self, current: Node, data):
-        if not self.head and not self.tail:
-            raise RuntimeError("Cannot insert afer non-existent node.")
-        if current == self.tail:
-            newNode = Node(data, current, None)
-            current.next = newNode
-            self.tail = newNode
-        else:
-            tmp = current.next
-            newNode = Node(data, current, current.next)
-            current.next = newNode
-            tmp.prev = newNode
-
-    def toList(self):
-        result = []
-        current = self.head
-        while current:
-            result.append(current.data)
-            current = current.next
-        return result
-
-    def toListRev(self):
-        result = []
-        current = self.tail
-        while current:
-            result.append(current.data)
-            current = current.prev
-        return result
-
-    def __repr__(self):
-        return "{}".format(self.head)
-
-
 def getInput():
     return utils.Inputstr("14")
 
@@ -90,26 +18,16 @@ def getPolymer(text):
 
 
 def step(counts, rules):
-    pass
+    result = {}
+
+    return result
 
 
 def polymerize(template, rules, steps=10):
-    polymer = DoubleLinkedList(list(template))
+    counts = Counter(template)
     for i in range(steps):
-        print("step: {}".format(i))
-        current = polymer.head.next
-        while current:
-            key = current.prev.data + current.data
-
-            if key in rules:
-                val = rules[key]
-                polymer.insert_after(current.prev, val)
-                current = current.next
-            else:
-                current = current.next
-
-    counts = Counter(polymer.toList()).values()
-    return max(counts) - min(counts), "".join(polymer.toList())
+        counts = step(counts, rules)
+    return max(counts.values()) - min(counts.values())
 
 
 example = """NNCB
@@ -148,81 +66,19 @@ Case = namedtuple(
 def test_polymerize_example():
     template, rules = getPolymer(example)
     cases = [
-        Case(1, 1, "NCNBCHB"),
-        Case(2, 5, "NBCCNBBBCBHCB"),
-        Case(3, 7, "NBBBCNCCNBBNBNBBCHBHHBCHB"),
-        Case(4, 18, "NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB"),
-        Case(10, 1588, ""),
-        # Case(40, 2188189693529, ""),
+        Case(1, 1),
+        Case(2, 5),
+        Case(3, 7),
+        Case(4, 18),
+        Case(10, 1588),
+        # Case(40, 2188189693529),
     ]
     for c in cases:
-        actual, p = polymerize(template, rules, c.steps)
-        # assert p == c.p
+        actual = polymerize(template, rules, c.steps)
         assert actual == c.expected, "Case: {}".format(c)
 
 
 def test_polymerize():
     template, rules = getPolymer(getInput())
-    actual, _ = polymerize(template, rules, 10)
+    actual = polymerize(template, rules, 10)
     assert actual == 2768
-
-
-def test_ddlist():
-    l = DoubleLinkedList()
-    l.prepend(1)
-    l.prepend(2)
-    l.prepend(3)
-    expected = [3, 2, 1]
-
-    assert l.toList() == expected
-    assert l.toListRev() == list(reversed(expected))
-
-
-def test_ddlist():
-    l = DoubleLinkedList([1, 2, 3])
-
-    expected = [1, 2, 3]
-    assert l.toList() == expected
-    assert l.toListRev() == list(reversed(expected))
-
-
-def test_ddlist_iter():
-    l = DoubleLinkedList()
-    l.prepend(1)
-    l.prepend(2)
-    l.prepend(3)
-    iterator = iter(l)
-    actual = [n.data for n in iterator]
-    expected = [3, 2, 1]
-
-    assert actual == expected
-
-
-def test_ddlist_insert_after():
-    l = DoubleLinkedList()
-    l.prepend(1)
-    l.prepend(2)
-    l.prepend(3)
-    iterator = iter(l)
-    first = next(iterator)
-    l.insert_after(first, 4)
-
-    actual = l.toList()
-    expected = [3, 4, 2, 1]
-    assert actual == expected
-
-
-def test_ddlist_insert_after2():
-    l = DoubleLinkedList()
-    l.prepend(1)
-    l.prepend(2)
-    l.prepend(3)
-    iterator = iter(l)
-    next(iterator)
-    next(iterator)
-    last = next(iterator)
-    l.insert_after(last, 4)
-
-    actual = l.toList()
-    expected = [3, 2, 1, 4]
-    assert actual == expected
